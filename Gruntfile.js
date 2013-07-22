dc50ffa226daa9482d464b8b1384fd35d788f9f4
Sean McCollum <anincrediblyshortname@yahoo.com>
2013-07-22 10:57:08 -0700

    Finishing up my tasks, starting to actually make something...barely.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,14 @@ module.exports = function (grunt) {
 					'main.css': 'scss/main.scss'
 				}
 			}
+		},
+		watch: {
+			files: ['./scss/*'],
+			tasks: ['default']
 		}
 	});
 	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	
 	grunt.registerTask('default', ['sass']);
 	
